2632cb8341b3c3083552497a7cf11d0f3fba1c6b
saheelsaneef <saneef1231@gmail.com>
2024-03-19 19:49:21 +0530

    added page

--- src/app/Components/cart.tsx
+++ src/app/Components/cart.tsx
@@ -1,16 +1,20 @@
 interface CartProps {
+  icon: string;
   title: string;
   value: string;
 }
 
-const Cart: React.FC<CartProps> = ({ title, value }) => {
+const Cart: React.FC<CartProps> = ({ icon, title, value }) => {
   return (
     <div className="w-[317px] h-[155px] relative">
       <div className="w-[317px] h-[155px] left-0 top-0 absolute bg-purple-400 rounded-[20px]" />
+      <div className="left-[13px]  absolute text-black text-[22px] font-semibold font-['Inter']">
+        {icon}
+      </div>
       <div className="left-[13px] top-[44px] absolute text-black text-[22px] font-semibold font-['Inter']">
         {title}
       </div>
-      <div className="left-[13px] top-[89px] absolute text-black text-2xl font-semibold font-['Inter']">
+      <div className="left-[13px] top-[88px] absolute text-black text-2xl font-semibold font-['Inter']">
         {value}
       </div>
     </div>
--- src/app/Components/dashboardcart.tsx
+++ src/app/Components/dashboardcart.tsx
@@ -3,9 +3,9 @@ import Cart from "./cart";
 const DashboardCart = () => {
   return (
     <div className="ml-[350px] flex gap-4 mt-[200px]">
-      <Cart title={"Total Income"} value={"240, 500.00 LKR"} />
-      <Cart title={"Total Order"} value={"324"} />
-      <Cart title={"Complains"} value={"04"} />
+      <Cart icon={"Mon"} title={"Total Income"} value={"240, 500.00 LKR"} />
+      <Cart icon={"Mon"} title={"Total Order"} value={"324"} />
+      <Cart icon={"Mon"} title={"Complains"} value={"04"} />
     </div>
   );
 };
