b7a1e1a26fd22c176a97542fe55bea5b1c89ca01
ASHANI <it22604194@my.sliit.lk>
2024-04-04 14:46:03 +0530

    my profile created

---
+++ src/app/Components/Customer/page.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (<div>
+    <div className="text-black text-[35px] font-bold font-['Work Sans']">My Account</div>
+<div className="w-[360px] h-[116px] bg-violet-200 shadow"></div>
+<div className="w-[141px] h-[38px] text-black text-xl font-normal font-['Inter']">   Information</div>
+<div className="w-[360px] h-[116px] bg-violet-200 shadow"></div>
+<div className="w-[360px] h-[116px] bg-violet-200 shadow"></div>
+<div className="w-[360px] h-[116px] bg-violet-200 shadow"></div>
+<div className="w-[360px] h-[116px] bg-violet-200 shadow"></div>
+<div className="w-[360px] h-[116px] bg-violet-200 shadow"></div>
+<div className="w-44 h-[39px] text-black text-xl font-normal font-['Inter']">      Old Orders</div>
+<div className="w-[204px] h-[39px] text-black text-xl font-normal font-['Inter']">          Addresses</div>
+<div className="w-[239px] h-[30px] text-black text-xl font-normal font-['Inter']">            Track Order</div>
+<div className="w-[275px] h-[47px] text-black text-xl font-normal font-['Inter']">   Order History and Details<br/></div>
+<div className="w-[218px] h-[29px] text-black text-xl font-normal font-['Inter']">            Vouchers</div>
+
+</div>
+  );
+}
---
+++ src/app/Components/Customerinfo/page.tsx
@@ -0,0 +1,26 @@
+
+import Link from 'next/link';
+
+export default function Page() {
+    return (
+      <div>
+        <h2>Customer Information</h2>
+        <div>
+          <strong>Name:</strong> 
+        </div>
+        <div>
+          <strong>Address:</strong>
+        </div>
+        <div>
+          <strong>Account Details:</strong>
+        </div>
+        <div>
+          <strong>Gender:</strong> 
+        </div>
+        {/* Add more details as needed */}
+      </div>
+    );
+  };
+  
+  
+  
