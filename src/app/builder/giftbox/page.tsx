78a2fb80a5543cb110a0dbbcfd11716a1d784ba0
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-04-17 01:28:48 +0530

    modified giftbox

--- src/app/builder/giftbox/page.tsx
+++ src/app/builder/giftbox/page.tsx
@@ -269,14 +269,8 @@ const GiftBoxSummary: React.FC = () => {
               selectedProduct.quantity > 0 && (
                 <tr key={index}>
                   <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 h-10 w-10">
-
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm font-medium text-gray-900">{selectedProduct.name}</div>
-                      </div>
-                    </div>
+                        <div className="text-sm font-medium text-gray-900">{selectedProduct.name}</div>                      
+                   
                   </td>
                   <td className="text-sm font-medium text-center text-gray-900">{selectedProduct.price}</td>
                   <td className="text-sm font-medium text-center text-gray-900">{selectedProduct.quantity}</td>
