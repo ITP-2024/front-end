0d8876a75b44ec7bdfba97ed782f7665badfd33b
ASHANI <it22604194@my.sliit.lk>
2024-05-02 17:01:24 +0530

    folder structure updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KpopShop",
@@ -6,14 +11,12 @@ export const metadata: Metadata = {
 };
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
-        </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
