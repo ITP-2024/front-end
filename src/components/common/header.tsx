f4afe8b5fd118057330122df5abd776a11a319da
Harsha <it22577306@my.sliit.lk>
2024-05-07 04:38:52 +0530

    refactor: Update header link in common/header.tsx

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
       </div>
 
       <div className="flex gap-5 justify-between my-auto mr-10">
-        <a href="#" >
+        <a href="/login" >
           <img
             loading="lazy"
             src="/image1.png"
