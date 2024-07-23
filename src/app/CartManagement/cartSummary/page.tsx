61cf58520830a554c837ef0c134c1acba172998d
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-07-23 19:49:03 +0530

    add product form created

--- src/app/CartManagement/cartSummary/page.tsx
+++
@@ -1,9 +0,0 @@
-// pages/cartSummary.tsx
-
-import CartSummaryPage from "../cSummary/page";
-
-const CartSummary: React.FC = () => {
-    return <CartSummaryPage />;
-};
-
-export default CartSummary;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,53 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Header from "@/components/common/header";
 import axios from "axios";
-import CartItem from '@/components/common/cart-item';
-import Footer from "@/components/common/footer";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Swal from 'sweetalert2';
+
+import { fetchReviews } from "@/api/review";
 
 interface Product {
-  id: string;
-  productId: string;
   name: string;
+  description: string;
   price: number;
   imageUrl: string;
-  quantity: number;
 }
 
-const Home: React.FC = () => {
+const Products: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
-  const [searchQuery, setSearchQuery] = useState<string>("");
-
-  const getQuantity = (productId: string): number => {
-    const selectedProduct = selectedProducts.find(
-      (product) => product.productId === productId
-    );
-    return selectedProduct ? selectedProduct.quantity : 0;
-  };
+  const router = useRouter();
 
   useEffect(() => {
-    axios
-      .get<Product[]>("http://localhost:8080/products")
-      .then((response) => {
-        setProducts(response.data);
+    // Fetch reviews from backend using fetchReviews function
+    fetchReviews()
+      .then((reviews) => {
+        setProducts(products);
       })
       .catch((error) => {
-        console.error("Error fetching gift box products:", error);
+        console.error("Error fetching products:", error);
       });
-
-    const storedSelectedProducts = localStorage.getItem("selectedProducts");
-    if (storedSelectedProducts) {
-      setSelectedProducts(JSON.parse(storedSelectedProducts));
-    }
   }, []);
 
-  const addToCart = (product: Product) => {
-    setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts, product]);
+  const handleAddProduct = () => {
+    // Redirect to add review page
+    router.push("/add-product");
   };
-
-  const handleClick = (product: Product) => {
-    const index = selectedProducts.findIndex(p => p.productId === product.productId);
-    if (index !== -1) {
-        const updatedProducts = [...selectedProducts];
-        updatedProducts[index] = { ...updatedProducts[index], quantity: updatedProducts[index].quantity + 1 };
-        setSelectedProducts(updatedProducts);
-    } else {
-        setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts, { ...product, quantity: 1 }]);
-    }
-    localStorage.setItem('selectedProducts', JSON.stringify(selectedProducts));
-  };
-
-  const handleSearch = (query: string) => {
-    setSearchQuery(query);
-  };
-
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
 
   return (
-    <div>
-      <Header />
-      <div className="p-4">
-        <input
-          type="text"
-          placeholder="Search products"
-          value={searchQuery}
-          onChange={(e) => handleSearch(e.target.value)}
-          className="border p-2 rounded w-full"
-        />
+    <div className="p-4">
+      <div className="p-4 flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Products</h1>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={handleAddProduct}
+        >
+          Add Product
+        </button>
       </div>
-      <section className="mx-auto max-w-7xl pb-16">
-        <ul className="flex flex-wrap">
-          {filteredProducts.map((product) => (
-            <CartItem
-              key={product.id}
-              product={{ ...product, quantity: getQuantity(product.id) }}
-              action={'Add to Cart'}
-              handleClick={handleClick}
-              isVisible={product.name.toLowerCase().includes(searchQuery.toLowerCase())}
-            />
-          ))}
-        </ul>
-      </section>
-      <Footer/>
+
     </div>
   );
 };
 
-export default Home;
+export default Products;
